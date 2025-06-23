e0107e4d47fbdacd1ad2c4d6069d9dcf8770780e
30Leo56 <info@exp-consult.de>
2025-06-23 16:14:57 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -58,9 +58,9 @@ export default function SkillMatrixSetup() {
 
   return (
     <div className="p-4 space-y-10">
-      <h1 className="text-xl font-bold">SkillMatrix NG - TEST</h1>
+      <h1 className="text-xl font-bold">SkillMatrix NG - TESST</h1>
 
-      <section className="w-full max-w-7xl mx-auto mb-12">
+      <section className="w-full mb-12 mt-8 px-4">
         <label htmlFor="projektbeschreibung" className="block text-sm font-semibold mb-2">
           Projektbeschreibung eingeben:
         </label>
